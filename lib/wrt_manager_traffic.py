8e17b09973ec32b725e9d82c0d3c8daa681248f5
fpemud <fpemud@sina.com>
2017-08-01 21:39:18 +0000

    fix

--- lib/wrt_manager_traffic.py
+++ lib/wrt_manager_traffic.py
@@ -3,6 +3,7 @@
 
 import os
 import logging
+import ipaddress
 import subprocess
 from wrt_util import WrtUtil
 
@@ -47,17 +48,41 @@ class WrtTrafficManager:
     def has_tfac_group(self, name):
         return name in self.tfacGroupDict
 
-    def add_tfac_group(self, name, priority, tfac_group):
-        assert name not in self.tfacGroupDict
+    def add_tfac_group(self, name, priority, facility_list):
+        # check
+        if name in self.tfacGroupDict:
+            raise Exception("traffic facility group %s already exists" % (name))
+        self._checkTrafficFacilityList(facility_list)
+
+        # record
         self.tfacGroupDict[name] = _TrafficFacilityGroup()
         self.tfacGroupDict[name].priority = priority
-        self.tfacGroupDict[name].facility_list = tfac_group
+        self.tfacGroupDict[name].facility_list = facility_list
 
-    def change_tfac_group(self, name, tfac_group):
-        self.tfacGroupDict[name].facility_list = tfac_group
+        # action
+        pass
+
+    def change_tfac_group(self, name, facility_list):
+        # check
+        if name not in self.tfacGroupDict:
+            raise Exception("traffic facility group %s does not exist" % (name))
+
+        # record
+        self.tfacGroupDict[name].facility_list = facility_list
+
+        # action
+        pass
 
     def remove_tfac_group(self, name):
+        # check
+        if name not in self.tfacGroupDict:
+            raise Exception("traffic facility group %s does not exist" % (name))
+
+        # record
         del self.tfacGroupDict[name]
+
+        # action
+        pass
 
     def on_wan_conn_up(self):
         WrtUtil.shell('/sbin/nft add rule wrtd natpost oifname %s masquerade' % (self.param.wanManager.wanConnPlugin.get_interface()))
@@ -103,17 +128,93 @@ class WrtTrafficManager:
         WrtUtil.forceDelete(self.pidFile)
         WrtUtil.forceDelete(self.cfgFile)
 
+    def _checkTrafficFacilityList(traffic_facility_list):
+        i = 1
+        for tfac in traffic_facility_list:
+            if "facility-name" not in tfac:
+                raise Exception("lacking \"facility-name\" for facility %d" % (i))
+
+            if "facility-type" not in tfac:
+                raise Exception("lacking \"facility-type\" for facility %s" % (tfac["facility-name"]))
+
+            if tfac["facility-type"] == "nameserver":
+                if "target" not in tfac:
+                    raise Exception("lacking \"target\" for facility %s" % (tfac["facility-name"]))
+                if not isinstance(tfac["target"], list):
+                    raise Exception("type of \"target\" is invalid for facility %s" % (tfac["facility-name"]))
+                for item in tfac["target"]:
+                    msg = "some element in \"target\" is invalid for facility %s" % (tfac["facility-name"])
+                    if not isinstance(item, list):
+                        raise Exception(msg)
+                    if len(item) != 2:
+                        raise Exception(msg)
+                    if not isinstance(item[0], str):
+                        raise Exception(msg)
+                    if not isinstance(item[1], int):
+                        raise Exception(msg)
+
+                if "domain-list" not in tfac:
+                    raise Exception("lacking \"domain-list\" for facility %s" % (tfac["facility-name"]))
+                if not isinstance(tfac["domain-list"], list):
+                    raise Exception("type of \"domain-list\" is invalid for facility %s" % (tfac["facility-name"]))
+                for item in tfac["domain-list"]:
+                    if not isinstance(item, str):
+                        raise Exception("some element in \"domain-list\" is invalid for facility %s" % (tfac["facility-name"]))
+
+                continue
+
+            if tfac["facility-type"] == "gateway":
+                if "target" not in tfac:
+                    raise Exception("lacking \"target\" for facility %s" % (tfac["facility-name"]))
+                msg = "invalid \"target\" for facility %s" % (tfac["facility-name"])
+                if not isinstance(tfac["target"], list):
+                    raise Exception(msg)
+                if len(tfac["target"]) != 2:
+                    raise Exception(msg)
+                if tfac["target"][0] is not None and not isinstance(tfac["target"][0], str):
+                    raise Exception(msg)
+                if tfac["target"][1] is not None and not isinstance(tfac["target"][1], str):
+                    raise Exception(msg)
+
+                if "network-list" not in tfac:
+                    raise Exception("lacking \"network-list\" for facility %s" % (tfac["facility-name"]))
+                if not isinstance(tfac["network-list"], list):
+                    raise Exception("type of \"network-list\" is invalid for facility %s" % (tfac["facility-name"]))
+                for item in tfac["network-list"]:
+                    msg = "some element in \"domain-list\" is invalid for facility %s" % (tfac["facility-name"])
+                    if not isinstance(item, str):
+                        raise Exception(msg)
+                    try:
+                        ipaddress.IPv4Network(item)
+                    except ipaddress.AddressValueError:
+                        raise Exception(msg)
+                    except ipaddress.NetmaskValueError:
+                        raise Exception(msg)
+                    except ValueError:
+                        raise Exception(msg)
+
+                continue
+
+            raise Exception("invalid \"facility-type\" for facility %s" % (tfac["facility-name"]))
+
+    def _changeTrafficFacilityList(oldList, newList):
+        # remove
+        for item in oldList:
+            if item["facility-name"] in [x["facility-name"] for x in newList]:
+                continue
+            if item["facility-type"] == "nameserver":
+                continue
+            elif item["facility-type"] == "gateway":
+                continue
+            else:
+                raise Exception
+
 
 class _TrafficFacilityGroup:
 
     def __init__(self):
         self.priority = None
         self.facility_list = []
-
-
-
-
-
 
         # # add nat rule
         # subprocess.check_call(["/sbin/nft", "add", "table", "ip", "cgfw"])
