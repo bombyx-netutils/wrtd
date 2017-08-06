03fb6914aee640a931abb15730e6e726e8d157e8
fpemud@sina.com <fpemud@sina.com>
2017-08-07 06:56:20 +0800

    fix

--- integration-test/dbus_GetRouterInfo.py
+++ integration-test/dbus_GetRouterInfo.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
 import dbus
+import json
 
 
 dbusObj = dbus.SystemBus().get_object('org.fpemud.WRT', '/org/fpemud/WRT')
-print(dbusObj.GetRouterInfo())
+str = json.dumps(json.loads(dbusObj.GetRouterInfo()), indent=4, sort_keys=True)
+print(str)
+
