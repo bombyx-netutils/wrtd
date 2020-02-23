6dc0522fdacb17ff6ae1f65b60d7d5094556b4d1
fpemud <fpemud@sina.com>
2020-02-23 22:56:21 +0800

    fix

--- lib/wrt_manager_wan.py
+++ lib/wrt_manager_wan.py
@@ -2,9 +2,12 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
 
 import os
+import shutil
 import signal
 import socket
 import logging
+import pyroute2
+import ipaddress
 from gi.repository import GLib
 from gi.repository import GObject
 from wrt_util import WrtUtil
@@ -25,6 +28,7 @@ class WrtWanManager:
         self.param = param
         self.logger = logging.getLogger(self.__module__ + "." + self.__class__.__name__)
 
+        self.wanConnPluginApi = None
         self.wanConnPlugin = None
 
         self.wanConnIpChecker = None
@@ -35,11 +39,10 @@ class WrtWanManager:
             cfgfile = os.path.join(self.param.etcDir, "wan-connection.json")
             if os.path.exists(cfgfile):
                 cfgObj = WrtUtil.loadJsonEtcCfg(cfgfile)
+                self.wanConnPluginApi = WanConnectionPluginApi(self, cfgObj["plugin"])
                 self.wanConnPlugin = self.param.pluginHub.getPlugin("wconn", cfgObj["plugin"])
-                tdir = os.path.join(self.param.tmpDir, "wconn-%s" % (cfgObj["plugin"]))
-                os.mkdir(tdir)
                 self.wanConnPlugin.init2(cfgObj,
-                                         tdir,
+                                         self.wanConnPluginApi.get_tmp_dir(),
                                          self.param.ownResolvConf,
                                          lambda: self.param.managerCaller.call("on_wan_conn_up"),
                                          lambda: self.param.managerCaller.call("on_wan_conn_down"))
@@ -52,6 +55,9 @@ class WrtWanManager:
                 self.wanConnPlugin.stop()
                 self.wanConnPlugin = None
                 self.logger.info("Internet connection deactivated.")
+            if self.wanConnPluginApi is not None:
+                self.wanConnPluginApi.dispose()
+                self.wanConnPluginApi = None
             raise
 
     def dispose(self):
@@ -60,6 +66,9 @@ class WrtWanManager:
             self.wanConnPlugin.stop()
             self.wanConnPlugin = None
             self.logger.info("Internet connection deactivated.")
+        if self.wanConnPluginApi is not None:
+            self.wanConnPluginApi.dispose()
+            self.wanConnPluginApi = None
         self.logger.info("Terminated.")
 
     def on_wan_conn_up(self):
@@ -114,3 +123,48 @@ class WrtWanManager:
         if self.wanConnIpChecker is not None:
             self.wanConnIpChecker.cancel()
             self.wanConnIpChecker = None
+
+
+class WanConnectionPluginApi:
+
+    def __init__(self, parent, pluginName):
+        self.parent = parent
+        self.tdir = os.path.join(self.parent.param.tmpDir, "wconn-%s" % (pluginName))
+        os.mkdir(self.tdir)
+
+    def dispose(self):
+        shutil.rmtree(self.tdir)
+
+    def get_tmp_dir(self):
+        return self.tdir
+
+    def activate_interface(self, ifname, ifconfig):
+        bnet = ipaddress.IPv4Network(ifconfig["prefix"], strict=False)
+
+        with pyroute2.IPRoute() as ipp:
+            idx = ipp.link_lookup(ifname=ifname)[0]
+            ipp.link("set", index=idx, state="up")
+            ipp.addr("add", index=idx, address=bnet.ip, mask=bnet.prefixlen, broadcast=str(bnet.broadcast_address))
+            if "gateway" in ifconfig:
+                ipp.route('add', dst="0.0.0.0/0", gateway=ifconfig["gateway"], oif=idx)
+            if "routes" in ifconfig:
+                for rt in ifconfig["routes"]:
+                    ipp.route('add', dst=rt["prefix"], gateway=rt["gateway"], oif=idx)
+
+        if "nameservers" in ifconfig:
+            with open(self.parent.param.ownResolvConf, "w") as f:
+                for ns in ifconfig["nameservers"]:
+                    f.write("nameserver %s\n" % (ns))
+
+        self.parent.param.managerCaller.call("on_wan_conn_up", ifname, ifconfig)
+
+    def deactive_interface(self, ifname):
+        with open(self.parent.param.ownResolvConf, "w") as f:
+            f.write("")
+
+        with pyroute2.IPRoute() as ipp:
+            idx = ipp.link_lookup(ifname="ifname")[0]
+            ipp.link("set", index=idx, state="down")
+            ipp.flush_addr(index=idx)
+
+        self.parent.param.managerCaller.call("on_wan_conn_down", ifname)
